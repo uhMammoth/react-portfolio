616621031c05afba9b29536a9f7291feadfa17d7
KyleP <kyl3p3tty@gmail.com>
2022-07-07 14:02:08 -0700

    got images to dynamically load

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,32 +1,32 @@
-export default[
+export default [
     {
         title: "proj 1",
         desc: "blah",
         github: "https://github.com/uhMammoth/GamePlatforms",
-        fileName: "logo.svg",
+        img: "logo.svg",
     },
     {
         title: "proj 2",
         desc: "blah",
         github: "https://github.com/uhMammoth/GamePlatforms",
-        fileName: "logo.svg",
+        img: "logo.svg",
     },
     {
         title: "proj 1",
         desc: "blah",
         github: "https://github.com/uhMammoth/GamePlatforms",
-        fileName: "logo.svg",
+        img: 'logo.svg',
     },
     {
         title: "proj 1",
         desc: "blah",
         github: "https://github.com/uhMammoth/GamePlatforms",
-        fileName: "logo.svg",
+        img: "logo.svg",
     },
     {
         title: "proj 1",
         desc: "blah",
         github: "https://github.com/uhMammoth/GamePlatforms",
-        fileName: "logo.svg",
+        img: "logo.svg",
     },
 ]
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-
-export default function ProjectCards(props){    
+export default function ProjectCards(props){        
     return( 
-        <a href={props.github}>
-            <div className="projectCard">            
-                <img src={`../assets/${props.fileName}`} alt={props.title} />
+        <a href={props.github} className="projectCard">
+            <img src={require(`../../public/images/${props.img}`)} alt={props.title} />
+            <div>                            
                 <h2 className="project-title">{props.title}</h2>
                 <p className="project-desc">{props.desc}</p>        
             </div> 
